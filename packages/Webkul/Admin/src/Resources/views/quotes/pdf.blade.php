ecc7ee0e8a7c8172ea7451bc57d89a0a54acf106
gustavopantoja <gustavopantoja.ap@gmail.com>
2024-07-14 17:53:22 -0300

    alteraçao de produto para servico

--- packages/Webkul/Admin/src/Resources/views/quotes/pdf.blade.php
+++ packages/Webkul/Admin/src/Resources/views/quotes/pdf.blade.php
@@ -1,249 +1,155 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-    <head>
-        <meta http-equiv="Cache-control" content="no-cache">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <title>Cotação</title>
+    <link href="https://fonts.googleapis.com/css?family=DejaVu+Sans&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'DejaVu Sans', sans-serif;
+            font-size: 12px;
+            color: #000;
+            background-color: #fff;
+            margin: 0;
+            padding: 20px;
+        }
 
-        <style type="text/css">
-            * {
-                font-family: DejaVu Sans;
-            }
+        .text-center {
+            text-align: center;
+        }
 
-            body, th, td, h5 {
-                font-size: 12px;
-                color: #000;
-            }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
 
-            .container {
-                padding: 20px;
-                display: block;
-            }
+        th, td {
+            border: 1px solid #d3d3d3;
+            padding: 5px 10px;
+        }
 
-            .quote-summary {
-                margin-bottom: 20px;
-            }
+        th {
+            background-color: #F4F4F4;
+            font-weight: bold;
+        }
 
-            .table {
-                margin-top: 20px;
-            }
+        td {
+            color: #3A3A3A;
+            vertical-align: middle;
+        }
 
-            .table table {
-                width: 100%;
-                border-collapse: collapse;
-                text-align: left;
-            }
+        .label {
+            font-weight: bold;
+        }
 
-            .table thead th {
-                font-weight: 700;
-                border-top: solid 1px #d3d3d3;
-                border-bottom: solid 1px #d3d3d3;
-                border-left: solid 1px #d3d3d3;
-                padding: 5px 10px;
-                background: #F4F4F4;
-            }
+        .logo {
+            height: 70px;
+            width: 70px;
+        }
 
-            .table thead th:last-child {
-                border-right: solid 1px #d3d3d3;
-            }
+        .sale-summary {
+            margin-top: 40px;
+            text-align: right;
+        }
 
-            .table tbody td {
-                padding: 5px 10px;
-                border-bottom: solid 1px #d3d3d3;
-                border-left: solid 1px #d3d3d3;
-                color: #3A3A3A;
-                vertical-align: middle;
-            }
+        .sale-summary td {
+            padding: 3px 5px;
+        }
 
-            .table tbody td p {
-                margin: 0;
-            }
+        .sale-summary .bold {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1 class="text-center">{{ __('admin::app.quotes.quote') }}</h1>
+</header>
 
-            .table tbody td:last-child {
-                border-right: solid 1px #d3d3d3;
-            }
+<section class="quote-summary">
+    <p><span class="label">{{ __('admin::app.quotes.quote-id') }} -</span> <span class="value">#{{ $quote->id }}</span></p>
+    <p><span class="label">{{ __('admin::app.quotes.quote-date') }} -</span> <span class="value">{{ $quote->created_at->format('d-m-Y') }}</span></p>
+    <p><span class="label">{{ __('admin::app.quotes.valid-until') }} -</span> <span class="value">{{ $quote->expired_at->format('d-m-Y') }}</span></p>
+    <p><span class="label">Data da Proposta -</span> <span class="value">{{ $quote->getAttribute('dt_proposta') ? $quote->getAttribute('dt_proposta')->format('d-m-Y') : 'Data não disponível' }}</span></p>
+    <p><span class="label">{{ __('admin::app.quotes.sales-person') }} -</span> <span class="value">{{ $quote->user->name }}</span></p>
+</section>
 
-           .sale-summary {
-                margin-top: 40px;
-                float: right;
-            }
+<table>
+    <thead>
+    <tr>
+        <th>Cliente Imediato</th>
+        @if ($quote->shipping_address)
+            <th>Cliente Final</th>
+        @endif
+    </tr>
+    </thead>
+    <tbody>
+    @if ($quote->billing_address)
+        <td>
+            <p>{{ $quote->billing_address['address'] }}</p>
+            <p>{{ $quote->billing_address['postcode'] . ' ' . $quote->billing_address['city'] }}</p>
+            <p>{{ $quote->billing_address['state'] }}</p>
+            <p>{{ $quote->billing_address['country'] ? core()->country_name($quote->billing_address['country']) : 'N/A' }}</p>
+        </td>
+    @endif
+    @if ($quote->shipping_address)
+        <td>
+            <p>{{ $quote->shipping_address['address'] }}</p>
+            <p>{{ $quote->shipping_address['postcode'] . ' ' . $quote->shipping_address['city'] }}</p>
+            <p>{{ $quote->shipping_address['state'] }}</p>
+            <p>{{ isset($quote->shipping_address['country']) ? core()->country_name($quote->shipping_address['country']) : 'N/A' }}</p>
+        </td>
+    @endif
+    </tbody>
+</table>
 
-            .sale-summary tr td {
-                padding: 3px 5px;
-            }
+<table class="items">
+    <thead>
+    <tr>
+        <th>UND</th>
+        <th>{{ __('admin::app.quotes.product-name') }}</th>
+        <th class="text-center">{{ __('admin::app.quotes.price') }}</th>
+        <th class="text-center">{{ __('admin::app.quotes.quantity') }}</th>
+        <th class="text-center">{{ __('admin::app.quotes.amount') }}</th>
+        <th class="text-center">{{ __('admin::app.quotes.tax') }}</th>
+        <th class="text-center">{{ __('admin::app.quotes.grand-total') }}</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($quote->items as $item)
+        <tr>
+            <td>{{ $item->sku }}</td>
+            <td>{{ $item->name }}</td>
+            <td class="text-center">{!! core()->formatBasePrice($item->price, true) !!}</td>
+            <td class="text-center">{{ $item->quantity }}</td>
+            <td class="text-center">{!! core()->formatBasePrice($item->total, true) !!}</td>
+            <td class="text-center">{!! core()->formatBasePrice($item->tax_amount, true) !!}</td>
+            <td class="text-center">{!! core()->formatBasePrice($item->total + $item->tax_amount, true) !!}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 
-            .sale-summary tr.bold {
-                font-weight: 600;
-            }
-
-            .label {
-                color: #000;
-                font-weight: bold;
-            }
-
-            .logo {
-                height: 70px;
-                width: 70px;
-            }
-
-            .text-center {
-                text-align: center;
-            }
-        </style>
-    </head>
-
-    <body style="background-image: none; background-color: #fff;">
-        <div class="container">
-
-            <div class="header">
-                <div class="row">
-                    <div class="col-12">
-                        <h1 class="text-center">{{ __('admin::app.quotes.quote') }}</h1>
-                    </div>
-                </div>
-
-                <div class="image">
-                    {{-- <img class="logo" src="{{ Storage::url(core()->getConfigData('sales.orderSettings.quote_slip_design.logo')) }}"/> --}}
-                </div>
-            </div>
-
-            <div class="quote-summary">
-                <div class="row">
-                    <span class="label">{{ __('admin::app.quotes.quote-id') }} -</span>
-                    <span class="value">#{{ $quote->id }}</span>
-                </div>
-
-                <div class="row">
-                    <span class="label">{{ __('admin::app.quotes.quote-date') }} -</span>
-                    <span class="value">{{ $quote->created_at->format('d-m-Y') }}</span>
-                </div>
-
-                <div class="row">
-                    <span class="label">{{ __('admin::app.quotes.valid-until') }} -</span>
-                    <span class="value">{{ $quote->expired_at->format('d-m-Y') }}</span>
-                </div>
-                <div class="row">
-                    <span class="label">Data da Proposta-</span>
-
-                    <span class="value">
-    {{ $quote->getAttribute('dt_proposta') ? $quote->getAttribute('dt_proposta')->format('d-m-Y') : 'Data não disponível' }}
-</span>
-                </div>
-
-                <div class="row">
-                    <span class="label">{{ __('admin::app.quotes.sales-person') }} -</span>
-                    <span class="value">{{ $quote->user->name }}</span>
-                </div>
-
-                <div class="table address">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th style="width: 50%">Cliente Imediato</th>
-
-                            @if ($quote->shipping_address)
-                                <th>Cliente Final</th>
-                            @endif
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        <tr>
-                            @if ($quote->billing_address)
-                                <td>
-                                    <p>{{ $quote->billing_address['address'] }}</p>
-                                    <p>{{ $quote->billing_address['postcode'] . ' ' .$quote->billing_address['city'] }} </p>
-                                    <p>{{ $quote->billing_address['state'] }}</p>
-                                    <p>{{ $quote->billing_address['country'] ? core()->country_name($quote->billing_address['country']) : 'N/A' }}
-                                    </p>
-                                </td>
-                            @endif
-
-                            @if ($quote->shipping_address)
-                                <td>
-                                    <p>{{ $quote->shipping_address['address'] }}</p>
-                                    <p>{{ $quote->shipping_address['postcode'] . ' ' .$quote->shipping_address['city'] }} </p>
-                                    <p>{{ $quote->shipping_address['state'] }}</p>
-                                    <p>{{ isset($quote->billing_address['country']) ? core()->country_name($quote->billing_address['country']) : 'N/A' }}
-                                    </p>
-                                </td>
-                            @endif
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-                <div class="table items">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>UND</th>
-
-                            <th>{{ __('admin::app.quotes.product-name') }}</th>
-
-                            <th class="text-center">{{ __('admin::app.quotes.price') }}</th>
-
-                            <th class="text-center">{{ __('admin::app.quotes.quantity') }}</th>
-
-                            <th class="text-center">{{ __('admin::app.quotes.amount') }}</th>
-
-
-                            <th class="text-center">{{ __('admin::app.quotes.tax') }}</th>
-
-                            <th class="text-center">{{ __('admin::app.quotes.grand-total') }}</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-
-                        @foreach ($quote->items as $item)
-                            <tr>
-                                <td>{{ $item->sku }}</td>
-
-                                <td>
-                                    {{ $item->name }}
-                                </td>
-
-                                <td>{!! core()->formatBasePrice($item->price, true) !!}</td>
-
-                                <td class="text-center">{{ $item->quantity }}</td>
-
-                                <td class="text-center">{!! core()->formatBasePrice($item->total, true) !!}</td>
-
-
-                                <td class="text-center">{!! core()->formatBasePrice($item->tax_amount, true) !!}</td>
-
-                                <td class="text-center">{!! core()->formatBasePrice($item->total + $item->tax_amount, true) !!}</td>
-                            </tr>
-                        @endforeach
-
-                        </tbody>
-                    </table>
-                </div>
-
-
-                <table class="sale-summary">
-                    <tr>
-                        <td>{{ __('admin::app.quotes.sub-total') }}</td>
-                        <td>-</td>
-                        <td>{!! core()->formatBasePrice($quote->sub_total, true) !!}</td>
-                    </tr>
-
-                    <tr>
-                        <td>{{ __('admin::app.quotes.tax') }}</td>
-                        <td>-</td>
-                        <td>{!! core()->formatBasePrice($quote->tax_amount, true) !!}</td>
-                    </tr>
-
-
-                    <tr>
-                        <td><strong>{{ __('admin::app.quotes.grand-total') }}</strong></td>
-                        <td><strong>-</strong></td>
-                        <td><strong>{!! core()->formatBasePrice($quote->grand_total, true) !!}</strong></td>
-                    </tr>
-                </table>
-
-            </div>
-
-        </div>
-    </body>
+<table class="sale-summary">
+    <tr>
+        <td>{{ __('admin::app.quotes.sub-total') }}</td>
+        <td>-</td>
+        <td>{!! core()->formatBasePrice($quote->sub_total, true) !!}</td>
+    </tr>
+    <tr>
+        <td>{{ __('admin::app.quotes.tax') }}</td>
+        <td>-</td>
+        <td>{!! core()->formatBasePrice($quote->tax_amount, true) !!}</td>
+    </tr>
+    <tr class="bold">
+        <td><strong>{{ __('admin::app.quotes.grand-total') }}</strong></td>
+        <td><strong>-</strong></td>
+        <td><strong>{!! core()->formatBasePrice($quote->grand_total, true) !!}</strong></td>
+    </tr>
+</table>
+</body>
 </html>
